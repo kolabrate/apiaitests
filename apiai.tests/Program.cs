a773e19f49448fc9321bbc4c25951626ecfb6a82
admin@kolabrate.com.au <Ana1982(>
2017-08-01 18:29:05 +1000

    Skeleton - basic response.

--- apiai.tests/Program.cs
+++ apiai.tests/Program.cs
@@ -10,10 +10,44 @@ namespace apiai.tests
 {
     class Program
     {
+
+        private static string accessToken = "787e0b5d351b4a06ba126e8ba0121a0c";
+        private static ApiAi apiAi;
         static void Main(string[] args)
         {
-            Console.Read();
+            try
+            {
+                RunAsync().Wait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.Read();
+                
+            }
+           
 
+        }
+
+        private static async Task RunAsync()
+        {
+            await GetResponse();
+        }
+
+        private static async Task<AIResponse> GetResponse()
+        {
+            var config = new AIConfiguration(accessToken, SupportedLanguage.English);
+            apiAi = new ApiAi(config);
+            var response = await apiAi.TextRequestAsync("Can I make an appoinment");
+            if (response.Status.Code == 200)
+            {
+
+                return response;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
