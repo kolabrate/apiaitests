498403fb45133517c710f5ef4e889c28ca7b29de
admin@kolabrate.com <Ana1982(>
2017-08-02 11:22:03 +1000

    Modified code - added originalrequest.

--- apiai.tests/Program.cs
+++ apiai.tests/Program.cs
@@ -11,8 +11,9 @@ namespace apiai.tests
     class Program
     {
 
-        private static string accessToken = "787e0b5d351b4a06ba126e8ba0121a0c";
+        private static string accessToken = "e0cdd76a123a48f194e626e15af15c02";
         private static ApiAi apiAi;
+        private static string input = "Can I make an appointment tomorrow at 9 AM for a haircut?";
         static void Main(string[] args)
         {
             try
@@ -38,9 +39,13 @@ namespace apiai.tests
 
         private static async Task<AIResponse> GetResponse()
         {
+            
             var config = new AIConfiguration(accessToken, SupportedLanguage.English);
-            apiAi = new ApiAi(config);
-            var response = await apiAi.TextRequestAsync("Can I make an appoinment");
+            var dataService = new AIDataService(config);
+            var request = new AIRequest(input);
+            request.OriginalRequest = PopulateOriginalRequest();
+            var response = await dataService.RequestAsync(request);
+           
             if (response.Status.Code == 200)
             {
 
@@ -50,6 +55,38 @@ namespace apiai.tests
             {
                 return null;
             }
+        }
+
+        private static OriginalRequest PopulateOriginalRequest()
+        {
+            
+            return new OriginalRequest()
+            {
+                Data = new
+                {
+                    ApiVersion = "",
+                    SmsSid = "",
+                    SmsStatus = "",
+                    SmsMessageSid = "",
+                    NumSegments = "",
+                    ToState = "",
+                    From = "+61400000000", //a temp phone number that represents its from console app.
+                    MessageSid = "SMf0000000000000000000000",//a rep of console.
+                    AccountSid = "",
+                    ToCity = "",
+                    FromCountry = "",
+                    ToZip = "",
+                    FromCity = "",
+                    To = "",
+                    FromZip = "",
+                    ToCountry = "",
+                    Body = input,
+                    NumMedia = "",
+                    FromState = ""
+                },
+                Source = "console"
+            };
+
         }
     }
 }
