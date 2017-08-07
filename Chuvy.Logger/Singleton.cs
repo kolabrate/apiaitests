5a4cbca4ea39184bb049accd229e63070170d0a2
admin@kolabrate.com <Ana1982(>
2017-08-07 22:32:50 +1000

    singleton class

---
+++ Chuvy.Logger/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//article for singleton.
+namespace Chuvy.Logger
+{
+    public sealed class Singleton //make sure the class is sealed as it must not be inherited.
+    {
+
+        private  Singleton() //keep the constructor private as this cant be initialised from outside.
+        {
+
+        }
+
+        public static Singleton InstanceCreation() 
+        {
+            
+            return new Singleton();
+        }
+
+    }
+}
