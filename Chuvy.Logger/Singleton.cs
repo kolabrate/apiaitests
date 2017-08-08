9be85e97cb5e88cd9c20dd1524815da2ecbc39fa
admin@kolabrate.com <Ana1982(>
2017-08-08 10:02:54 +1000

    Chuvy Logger

--- Chuvy.Logger/Singleton.cs
+++ Chuvy.Logger/Singleton.cs
@@ -3,23 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// article - https://dzone.com/articles/understanding-and-implementing-singleton-pattern-i
+// profiler - https://marketplace.visualstudio.com/items?itemName=JetBrains.dotTrace
 
 
-//article for singleton.
 namespace Chuvy.Logger
 {
     public sealed class Singleton //make sure the class is sealed as it must not be inherited.
     {
-
-        private  Singleton() //keep the constructor private as this cant be initialised from outside.
+        private int _counter;
+        private Singleton() //keep the constructor private as this cant be initialised from outside.
         {
 
         }
-
-        public static Singleton InstanceCreation() 
+        private static readonly Singleton Instance = null;
+        static Singleton()
         {
-            
-            return new Singleton();
+            Instance = new Singleton();
+        }
+        public static Singleton GetInstance() //is it thread safe. -- check if the instance is already null.
+        {
+            return Instance;
+          // return new Singleton();
+        }
+
+        public int ReturnCounter()
+        {
+            return ++_counter;
         }
 
     }
